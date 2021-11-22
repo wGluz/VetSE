11713220fdf43633b2e843996ec571e2e0bb1945
wGluz <94722395+wGluz@users.noreply.github.com>
2021-11-22 19:23:47 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/vetse/VetSE_Bem_Vindo.java
@@ -0,0 +1,14 @@
+package com.example.vetse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VetSE_Bem_Vindo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vet_se_bem_vindo);
+    }
+}
---
+++ app/src/main/java/com/example/vetse/VetseCrieUmaConta.java
@@ -0,0 +1,14 @@
+package com.example.vetse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VetseCrieUmaConta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vetse_crie_uma_conta);
+    }
+}
