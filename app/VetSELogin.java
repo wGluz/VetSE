9b6b5fee20578adfcf4eb4a46dbf1feab7730573
WELTONCS <93778567+Weltocs@users.noreply.github.com>
2021-12-01 00:04:22 -0300

    Add files via upload

---
+++ app/VetSELogin.java
@@ -0,0 +1,43 @@
+package com.example.vetse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class VetSELogin extends AppCompatActivity {
+
+    private TextView text_cadastre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vetse_login);
+
+
+        getSupportActionBar().hide();
+
+        IniciarComponentes();
+
+        text_cadastre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(VetSELogin.this, VetseCrieUmaConta.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+        private void IniciarComponentes() {
+        text_cadastre = findViewById(R.id.cadastre);
+
+
+    }
+
+}
+
+
+
---
+++ app/VetSE_Bem_Vindo.java
@@ -0,0 +1,20 @@
+package com.example.vetse;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class VetSE_Bem_Vindo extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vet_se_bem_vindo);
+        getSupportActionBar() .hide();
+
+
+    }
+}
