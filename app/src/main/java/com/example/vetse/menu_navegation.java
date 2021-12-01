46686a775ebd13e660853b85be0d0afb03656d6b
danieln18 <ddanielsouza200@gmail.com>
2021-11-30 23:41:36 -0300

    finished menu layout

---
+++ app/src/main/java/com/example/vetse/menu_navegation.java
@@ -0,0 +1,14 @@
+package com.example.vetse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_navegation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_navegation);
+    }
+}
